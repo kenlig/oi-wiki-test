49d24d3f78f6673114c0897638fc42d75a87af68
kenlig <28685375+kenlig@users.noreply.github.com>
2021-08-05 21:53:40 +0800

    init

--- docs/string/code/trie/trie_1.cpp
+++ docs/string/code/trie/trie_1.cpp
@@ -1,9 +1,9 @@
 #include <cstdio>
-#include <cstring>
+
 const int N = 500010;
 
 char s[60];
-int n, m, ch[N][30], tag[N], tot = 1;
+int n, m, ch[N][26], tag[N], tot = 1;
 
 int main() {
   scanf("%d", &n);
@@ -11,13 +11,12 @@ int main() {
   for (int i = 1; i <= n; ++i) {
     scanf("%s", s + 1);
     int u = 1;
-    int len=strlen(s); 
-    for (int j = 1; j <= len; ++j) {
+    for (int j = 1; s[j]; ++j) {
       int c = s[j] - 'a';
       if (!ch[u][c]) ch[u][c] = ++tot;// 如果这个节点的子节点中没有这个字符，添加上并将该字符的节点号记录为++tot 
       u = ch[u][c];// 往更深一层搜索 
     }
-    tag[u] = 0;// 最后一个字符为节点 u 的名字未被访问到记录为 0
+    tag[u] = 1;// 最后一个字符为节点 u 的名字未被访问到记录为 1
   }
 
   scanf("%d", &m);
@@ -30,10 +29,10 @@ int main() {
       u = ch[u][c];
       if (!u) break;  // 不存在对应字符的出边说明名字不存在
     }
-    if (tag[u] == 0) {
-      tag[u] = 1;// 最后一个字符为节点 u 的名字已经被访问 
+    if (tag[u] == 1) {
+      tag[u] = 2;// 最后一个字符为节点 u 的名字已经被访问 
       puts("OK");
-    } else if (tag[u] == 1)//已经被访问，重复访问 
+    } else if (tag[u] == 2)//已经被访问，重复访问 
       puts("REPEAT");
     else
       puts("WRONG");
